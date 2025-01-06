cb55ac5ab97dba705aace58cc7c256ca6d29997c
JM <pizzashim@gmail.com>
2025-01-06 13:33:04 +0900

    change commit

--- src/main/java/org/example/RPSGame.java
+++ src/main/java/org/example/RPSGame.java
@@ -1,12 +1,26 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class RPSGame {
 
-    public static void main(String[] args) {
-        // 사용자 값
-        int user = 2;
-        // com 값
-        int com = 0;
+    public static void main(String[] args) throws IOException {
+        /*BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 사용자 값 0,1,2
+        int user = Integer.parseInt(br.readLine());*/
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("가위0, 바위1, 보자기2 입력하세요 :");
+
+        String str = sc.nextLine(); // 키보드에서 입력한 문자열
+        System.out.println(str);
+
+        int user = Integer.parseInt(str);
+        // com 값 0,1,2
+        int com = (int)(Math.random() * 3);
 
         /*user = user < com ? user + 3 : user*/;
         if (user < com) {
@@ -14,6 +28,9 @@ public class RPSGame {
         } // end if
 
         int result = user - com; // 0,1,2
+
+        System.out.println("USER: " + user);
+        System.out.println("COM: " + com);
 
         switch (result) {
             case 0:
